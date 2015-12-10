9e4a19198241e824509c8b7962ce0c176f45ca7b
BrotherSeamus <jamespkirwin@gmail.com>
2015-12-10 10:03:27 -0600

    added wishlist display

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -24,15 +24,25 @@ app.controller('channelCtrl', ['$scope', '$http', function ($scope, $http) {
 app.controller('wishCtrl', ['$scope', '$http', function($scope, $http) {
 	$scope.message = "What Slack channels would you like to see in the future?";
 
+	$http({
+		url: '/channels/wishlist',
+		method: 'get'
+	}).then(function(res) {
+		$scope.channels = res.data;
+	});
+
 	$scope.submit = function () {
-		var channel = {name: $scope.newChannel};
+		var channel = {
+			name: $scope.newChannel,
+			purpose: $scope.purpose
+		};
 
 		$http({
 			url: '/channels',
 			method: 'post',
 			data: JSON.stringify({newChannel: channel})
 		}).then(function(res) {
-			alert('Bam! New Channel!');
+			$scope.channels = res.data;
 		});
 	}
 
--- routes/channels.js
+++ routes/channels.js
@@ -4,7 +4,15 @@ var db = require('../data/mongo.js');
 
 /* GET channel listing. */
 router.get('/', function(req, res, next) {
-  var results = db.channels.find();
+  var results = db.channels.find({purpose:{$exists:false}});
+  var array = results.toArray(function(err, arr) {
+    var doc = arr;
+    res.send(doc);
+  });
+});
+
+router.get('/wishlist', function(req, res, next) {
+  var results = db.channels.find({purpose:{$exists:true}});
   var array = results.toArray(function(err, arr) {
     var doc = arr;
     res.send(doc);
@@ -15,7 +23,11 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
   db.channels.insert(req.body.newChannel);
   console.log(req.body.newChannel);
-  res.send(req.body.newChannel);
+  var results = db.channels.find({purpose:{$exists:true}});
+  var array = results.toArray(function(err, arr) {
+    var doc = arr;
+    res.send(doc);
+  });
 })
 
 module.exports = router;
